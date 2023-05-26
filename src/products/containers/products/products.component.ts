c4d2bfed5d8ad6b1c6583664e2514f67abb2937c
BBCA <boriscavalier@eurofins.com>
2023-05-26 14:07:20 +0200

    - using Observable<Pizza[]> & rxjs async

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {Pizza} from '../../models/pizza.model';
 import {Store} from '@ngrx/store';
@@ -8,36 +8,34 @@ import * as fromStore from '../../store';// keep in mind, from module is a folde
   selector: 'products',
   styleUrls: ['products.component.scss'],
   template: `
-    <div class="products">
-      <div class="products__new">
-        <a
-          class="btn btn__ok"
-          routerLink="./new">
-          New Pizza
-        </a>
+      <div class="products">
+          <div class="products__new">
+              <a class="btn btn__ok"
+                 routerLink="./new">
+                  New Pizza
+              </a>
+          </div>
+          <div class="products__list">
+              <div *ngIf="!((pizzas$ | async)?.length)">
+                  No pizzas, add one to get started.
+              </div>
+              <pizza-item
+                      *ngFor="let pizza of (pizzas$ |async)"
+                      [pizza]="pizza">
+              </pizza-item>
+          </div>
       </div>
-      <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
-          No pizzas, add one to get started.
-        </div>
-        <pizza-item
-          *ngFor="let pizza of (pizzas)"
-          [pizza]="pizza">
-        </pizza-item>
-      </div>
-    </div>
   `,
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+  pizzas$: Observable<Pizza[]>;
 
   constructor(private store: Store<fromStore.ProductsState>) {
   }
 
   ngOnInit() {
-    this.store.select<any>(fromStore.getPizzas).subscribe({
-        next: (state) => console.log(state)
-      }
-    )
+    // command below is commented and replaced by command after-prettier using a stream
+    // this.store.select<Pizza[]>(fromStore.getPizzas).subscribe({next:(state)=> this.pizzas = state});
+    this.pizzas$ = this.store.select<Pizza[]>(fromStore.getPizzas);
   }
 }
