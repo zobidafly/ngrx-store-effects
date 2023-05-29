8dce5367a36daec2e86e645284600d998a78678f
BBCA <boriscavalier@eurofins.com>
2023-05-29 17:43:32 +0200

    prettify

--- src/products/store/effects/pizzas.effects.ts
+++ src/products/store/effects/pizzas.effects.ts
@@ -1,9 +1,11 @@
 import {Injectable} from "@angular/core";
 import {Effect, Actions} from "@ngrx/effects";
-import * as pizzaActions from '../actions/pizzas.actions';
 import {catchError, map, switchMap} from "rxjs/operators";
-import * as fromServices from '../../services';
 import {of} from "rxjs/observable/of";
+
+import * as pizzaActions from '../actions/pizzas.actions';
+import * as fromServices from '../../services';
+import {Pizza} from "../../models/pizza.model";
 
 @Injectable()
 export class PizzasEffects {
@@ -15,7 +17,7 @@ export class PizzasEffects {
   loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS).pipe(
     switchMap(() => {
       return this.pizzaService.getPizzas().pipe(
-        map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
+        map((pizzas:Pizza[]) => new pizzaActions.LoadPizzasSuccess(pizzas)),
         catchError(error => of(new pizzaActions.LoadPizzasFail(error)))
       )
     })
