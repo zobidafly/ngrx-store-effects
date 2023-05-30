a42101adafac7b750e35100cf4ffed45ebd01a8e
BBCA <boriscavalier@eurofins.com>
2023-05-30 10:32:25 +0200

    create CustomRouterStateSerializer

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,6 +1,6 @@
 import * as fromRouter from '@ngrx/router-store';
 import {ActionReducerMap, createFeatureSelector} from '@ngrx/store';
-import {Params} from "@angular/router";
+import {Params, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 
 export interface RouterStateUrl {
   url: string;
@@ -17,3 +17,17 @@ export const reducers: ActionReducerMap<State> = {
 }
 
 export const getRouterState = createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>("routerReducer");
+
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const {url} = routerState;//takes url from routerState
+    const {queryParams} = routerState.root;//takes queryParams from routerState.root
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+    const {params} = state;
+    return {url, queryParams, params};
+  }
+}
