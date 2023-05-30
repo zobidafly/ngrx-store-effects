6c5bba2af0286ce53825bee97d5a60954214689e
BBCA <boriscavalier@eurofins.com>
2023-05-30 12:42:30 +0200

    considering routerStore into selectors to add a new selector: getSelectedPizza change path:id into path:pizzaId in productModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -24,11 +24,11 @@ export const ROUTES: Routes = [
     component: fromContainers.ProductsComponent,
   },
   {
-    path: ':id',
+    path: 'new',
     component: fromContainers.ProductItemComponent,
   },
   {
-    path: 'new',
+    path: ':pizzaId',
     component: fromContainers.ProductItemComponent,
   },
 ];
--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -1,11 +1,18 @@
 import {createSelector} from "@ngrx/store";
 
+import * as fromRoot from "../../../app/store";
 import * as fromFeature from "../reducers";
 import * as fromReducers from "../reducers/pizzas.reducers";
+import {Pizza} from "../../models";
 
 export const getPizzaState = createSelector(fromFeature.getProductsState, (state: fromFeature.ProductsState) => state.pizzas);
 export const getPizzaEntities = createSelector(getPizzaState, fromReducers.getPizzaEntities);
-export const getAllPizzas = createSelector(getPizzaEntities, (entities:fromReducers.PizzaEntities)=> Object.values(entities));
+export const getAllPizzas = createSelector(getPizzaEntities, (entities: fromReducers.PizzaEntities) => Object.values(entities));
+export const getSelectedPizza =
+  createSelector(getPizzaEntities,
+    fromRoot.getRouterState,
+    (entities, router): Pizza => router.state && entities[router.state.params.pizzaId]
+  );
 export const getAllPizzas_Alternate = createSelector(getPizzaEntities, (entities: fromReducers.PizzaEntities) => Object.keys(entities).map(pizzaId => entities[parseInt(pizzaId, 10)]));
 export const getPizzasLoading = createSelector(getPizzaState, fromReducers.getPizzasLoading);
 export const getPizzasLoaded = createSelector(getPizzaState, fromReducers.getPizzasLoaded);
